12df4b47caa487d8728f207dffe50f785d97f0bf
Aman0835 <darkestnight9029@gmail.com>
2026-07-23 22:37:11 +0530

    Update api.js

--- Admin/Frontend/src/services/api.js
+++ Admin/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
 
@@ -29,4 +29,4 @@ api.interceptors.response.use(
   }
 );
 
-export default api;
+export default api;
